544e71a530e3d8321cbb8811bf18e293ff2156c6
joegoyom2 <134905059+joegoyom2@users.noreply.github.com>
2025-03-25 00:38:40 +0800

    Update external_ai_config.py

--- web_app/config/external_ai_config.py
+++ web_app/config/external_ai_config.py
@@ -9,3 +9,15 @@ def get_openai():
     api_version="2024-02-01"
 )
     return client
+
+def get_prompt(document_type: str) -> str:
+    """
+    Returns a prompt to generate a PlantUML diagram of a given type.
+    document_type: "use case diagram", "sequence diagram", etc.
+    """
+    return f"""From the given file, the data inside are metadata extracted from a project, 
+the data includes information of the actual code. From the relationship of the data, 
+please try to draw a {document_type} to illustrate the design of the project. 
+The graph should be in the format of PlantUML with an older version. 
+Please only send back the PlantUML code without any explanation.
+"""
