92dbc5758b0e143cbeec505af02d6ed0b86de035
matthew41308 <matthew.matthew899@gmail.com>
2025-04-30 04:57:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,21 +26,6 @@ if ! ssh-keygen -yf /dev/stdin >/dev/null 2>&1 < ~/.ssh/id_ecdsa ; then
 fi
 echo "key OK"
 
-
-
-# Build a local .ssh folder on Render
-mkdir -p ~/.ssh
-
-# Add GitHub as a known host so it does not prompt you to add
-# GitHub's IP address to known_hosts
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts 2> /dev/null
-
-# Copy the secret file to the local .ssh folder and chmod it.
-# This grabs the SSH_KEY from the environment group and
-# reverses the Base64 encoding to transform it back into a file
-echo "$SSH_KEY" | base64 -d > ~/.ssh/id_ecdsa
-chmod 400 ~/.ssh/id_ecdsa
-
 # ---------- choose a free local port -------------------------------------
 MYSQL_TUNNEL_PORT=$(python - <<'PY'
 import socket, json, os
